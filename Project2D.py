d8abfa9fb58fe372764839c14bd6b14d809ccd77
SangJun Cho <fpdltm456@gmail.com>
2022-10-16 22:24:51 +0900

    Title + Pause

--- Project2D.py
+++ Project2D.py
@@ -1,6 +1,8 @@
-
-
+import pico2d
 from pico2d import *
+import game_framework
+import title
+import PAUSE
 
 class Screen:
     def __init__(self):
@@ -21,6 +23,7 @@ class Grass:
             self.x = 15
             while self.x < 800:
                 self.image.clip_draw(0, 0, 30, 30, self.x, self.y)
+
                 self.x += 30
             self.y -= 30
         self.x = 15
@@ -42,41 +45,43 @@ class Cat:
 
 
     def update(self):  # 소년의 행위 구현
-        global running
-        events = get_events()
-        for event in events:
-            if event.type == SDL_QUIT:
-                running = False
-            elif event.type == SDL_KEYDOWN:
-                if event.key == SDLK_a:
-                    self.dir = -1
-
-
-                elif event.key == SDLK_d:
-                    self.dir = 1
-
-
-                # elif event.key == SDLK_w:
-                #     self.dir2 += 1
-
-
-                elif event.key == SDLK_ESCAPE:
-                    running = False
-            elif event.type == SDL_KEYUP:
-                if event.key == SDLK_a:
-                    self.dir = 0
-
-                    self.pos = 0
-
-
-                elif event.key == SDLK_d:
-                    self.dir = 0
-
-                    self.pos = 1
+        # global running
+        # events = get_events()
+        # for event in events:
+        #     if event.type == SDL_QUIT:
+        #         game_framework.quit()
+        #     elif event.type == SDL_KEYDOWN:
+        #         if event.key == SDLK_a:
+        #             self.dir = -1
+        #
+        #
+        #         elif event.key == SDLK_d:
+        #             self.dir = 1
+        #
+        #
+        #         # elif event.key == SDLK_w:
+        #         #     self.dir2 += 1
+        #         elif event.key == SDLK_p:
+        #             game_framework.push_state(pause)
+        #
+        #
+        #         elif event.key == SDLK_ESCAPE:
+        #             game_framework.quit()
+        #     elif event.type == SDL_KEYUP:
+        #         if event.key == SDLK_a:
+        #             self.dir = 0
+        #
+        #             self.pos = 0
+        #
+        #
+        #         elif event.key == SDLK_d:
+        #             self.dir = 0
+        #
+        #             self.pos = 1
 
 
         # self.x += dir * 5
-        self.x += self.dir * 5
+        self.x += self.dir * 1
         # self.y += self.dir2 * 5
         self.frame = self.frame % 2
 
@@ -114,46 +119,138 @@ class Cat:
 #                 dir += 1
 #             elif event.key == SDLK_d:
 #                 dir -= 1
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_a:
+                cat.dir = -1
+
+
+            elif event.key == SDLK_d:
+                cat.dir = 1
+
+
+            # elif event.key == SDLK_w:
+            #     cat.dir2 += 1
+
+            elif event.key == SDLK_ESCAPE:
+                game_framework.quit()
+            elif event.key == SDLK_p:
+                game_framework.push_state(PAUSE)
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_a:
+                cat.dir = 0
+
+                cat.pos = 0
+
+
+            elif event.key == SDLK_d:
+                cat.dir = 0
+                cat.pos = 1
 
 
 
 
 # initialization code
-open_canvas()
-screen = Screen()
+# open_canvas()
+# screen = Screen()
+# # dir = 0
+# grass = Grass()  # 잔디 객체를 생성
+# cat = Cat()
+# count = 0
+# # List Compreshension
+
+# open_canvas()
+screen = None
 # dir = 0
-grass = Grass()  # 잔디 객체를 생성
-boy = Cat()
+grass = None  # 잔디 객체를 생성
+cat = None
 count = 0
-# List Compreshension
-
-
 running = True
 
-# game main loop code
-while running:
-    # handle_events()  # 키 입력 받아들이는 처리..'
-    # Game logic
-    # grass 에 대한 상호작용.
-    # boy.update()
-    # boy.update() # 소년의 상호작용.
+def enter():
+    global cat, grass, running, screen
+    cat = Cat()
+    grass = Grass()
+    screen = Screen()
+    running = True
 
-    # Game drawing
-    clear_canvas()
+def exit():
+    global cat, grass, screen
+    del cat
+    del grass
+    del screen
+
+def update():
+    cat.update()
+
+def draw_world():
     screen.draw()
     grass.draw()
-    boy.update()
+    cat.draw()
 
-    boy.draw()
-    if dir != 0:
+def draw():
+    # 게임 월드 렌더링
+    global count
+    clear_canvas()
+    draw_world()
+    screen.draw()
+    grass.draw()
+    cat.draw()
+    if cat.dir != 0:
         count += 1
-        if count % 10 == 0:
-            boy.frame += 1
-
+        if count % 100 == 0:
+            cat.frame += 1
     update_canvas()
 
-    get_events()
-    # boy.draw()
+def pause():
+    pass
 
 
-    delay(0.01)
+def resume():
+
+    pass
+
+# game main loop code
+# while running:
+#     handle_events()  # 키 입력 받아들이는 처리..'
+#     # Game logic
+#     # grass 에 대한 상호작용.
+#     # boy.update()
+#     # boy.update() # 소년의 상호작용.
+#
+#     # Game drawing
+#     clear_canvas()
+#     screen.draw()
+#     grass.draw()
+#     cat.update()
+#
+#     cat.draw()
+#     if cat.dir != 0:
+#         count += 1
+#         if count % 10 == 0:
+#             cat.frame += 1
+#
+#     update_canvas()
+#
+#     get_events()
+#     # boy.draw()
+#
+#
+#     delay(0.01)
+
+def test_self():
+    import sys
+    this_module = sys.modules['__main__']
+    pico2d.open_canvas()
+    game_framework.run(this_module)
+    pico2d.close_canvas()
+
+
+
+if __name__ == '__main__': # 만약 단독 실행 상태이면,
+    test_self()
---
+++ title.py
@@ -0,0 +1,56 @@
+from pico2d import *
+import game_framework
+import Project2D
+
+image = None
+
+def enter():
+    global image
+    image = load_image('title.png')
+    pass
+
+def exit():
+    global image
+    del image
+
+    pass
+
+
+
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
+            game_framework.change_state(Project2D)
+def draw():
+    clear_canvas()
+    image.draw(400, 300)
+    update_canvas()
+    pass
+def update():
+
+    pass
+
+
+def pause():
+    pass
+
+def resume():
+
+    pass
+
+def test_self():
+    import sys
+    this_module = sys.modules['__main__']
+    pico2d.open_canvas()
+    game_framework.run(this_module)
+    pico2d.close_canvas()
+
+
+
+if __name__ == '__main__': # 만약 단독 실행 상태이면,
+    test_self()
